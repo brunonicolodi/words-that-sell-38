e7ffefaabdf1b124f5cd734246d63015d12a3734
brunonicolodi <bwnicolodi@gmail.com>
2026-04-26 12:01:06 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,19 +5,18 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-base: '/palavras-que-vendem-38/',
+  base: '/palavras-que-vendem-38/',
   server: {
     host: "::",
     port: 8080,
-    hmr: {
-      overlay: false,
-    },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-    dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
   },
 }));
