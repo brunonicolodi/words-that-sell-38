e9f9e16cbeab8693b851b36cfde62d81f669c083
brunonicolodi <bwnicolodi@gmail.com>
2026-04-26 11:37:14 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/palavras-que-vendem-38/',
+base: '/palavras-que-vendem-38/',
   server: {
     host: "::",
     port: 8080,
